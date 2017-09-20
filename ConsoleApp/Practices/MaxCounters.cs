348723545994df209052418adcf63413c87bda6c
iubarif <iubarif@yahoo.com>
2017-09-20 01:45:16 -0700

    maxCounter

---
+++ ConsoleApp/Practices/MaxCounters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Practices
+{
+    //  https://codility.com/demo/take-sample-test/max_counters
+    //  Correctness: 100% performance: 40% Score:66% Duration: 40 min
+
+    public class MaxCounters
+    {
+        public int[] solution(int N, int[] A) {
+
+            int[] counter = new int[N];
+            var max = 0;
+
+            for (int i = 0; i < A.Length; i++) {
+                if (A[i] > N)
+                {
+                    counter = Enumerable.Repeat(max, N).ToArray();
+                }
+                else {
+                    var couterVal = counter[A[i]-1];
+
+                    couterVal++;
+
+                    counter[A[i]-1] = couterVal;
+
+                    if(couterVal> max)
+                    {
+                        max = couterVal;
+                    }
+                }
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ UnitTestProject/MaxCountersTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp.Practices;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class MaxCountersTest
+    {
+        MaxCounters _mCounter; 
+        public MaxCountersTest()
+        {
+            _mCounter = new MaxCounters();
+        }
+
+        [TestMethod]
+        public void MaxCountersTest_Given_Returns_32242()
+        {
+            int N = 5;
+            int[] A = { 3,4,4,6,1,4,4};
+
+            int[] RetVal = { 3, 2, 2, 4, 2 };
+
+            CollectionAssert.AreEquivalent(_mCounter.solution(N, A), RetVal);
+        }
+    }
+}
