0f00ff10be53cd79fb8d4a56e36787cb5689ba07
Arif Mosharraf <arif.mosharraf@hssbc.ca>
2017-09-22 13:27:53 -0700

    CountDiv added

---
+++ ConsoleApp/Practices/CountDiv.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Practices
+{
+	//	https://codility.com/demo/take-sample-test/count_div
+	//	Correctness: 100% performance: 00% Score:50% Duration: 30 min
+	public class CountDiv
+	{
+		int count = 0;
+		
+		public int solution(int A, int B, int K) {
+			
+			getCount(A,B,K);
+
+			return count;
+		}
+
+		public void getCount(int a, int b, int k)
+		{
+			if (a <= b)
+			{
+				if (a%k == 0)
+				{
+					count++;					
+				}
+
+				a++;
+				getCount(a, b, k);
+			}			
+		}
+
+	}
+}
---
+++ UnitTestProject/CountDivTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp.Practices;
+
+namespace UnitTestProject
+{
+	[TestClass]
+	public class CountDivTest
+	{
+		CountDiv _cd;
+
+		public CountDivTest()
+		{
+			_cd = new CountDiv();
+		}
+
+		[TestMethod]
+		public void CountDivTest_6_11_2_Returns_3()
+		{
+			Assert.AreEqual(_cd.solution(6, 11, 2), 3);
+		}
+
+		[TestMethod]
+		public void CountDivTest_11_345_17_Returns_20()
+		{
+			Assert.AreEqual(_cd.solution(11, 345, 17), 20);
+		}		
+	}
+}
